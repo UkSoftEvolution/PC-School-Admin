faf84e4d236eb94a035c8230e65a9651c02ab251
GreshnikAlex <1995BLADE@mail.ru>
2019-02-18 20:07:30 +0200

    - Реализация главного окна - Добавление стлей цветов - Опции главного окна - Патерн MVVM

---
+++ Other/RelayCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace PC_School_Admin.Other
+{
+    /// <summary>
+    /// Класс для работы с командами в MVVM
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter) => canExecute == null || canExecute(parameter);
+        public void Execute(object parameter) => execute(parameter);
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using PC_School_Admin.Other;
+using System.Windows.Controls;
+
+namespace PC_School_Admin.ViewModel
+{
+    /// <summary>
+    /// ViewModel: Класс для Binding с MainView
+    /// </summary>
+    public class MainViewModel : MVVM
+    {
+        #region Fields
+        Page active; //Активная страница
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Активная страница
+        /// </summary>
+        public Page Active
+        {
+            get => active;
+            set
+            {
+                active = value;
+                OnPropertyChanged(nameof(active));
+            }
+        }
+
+        #endregion
+    }
+}
