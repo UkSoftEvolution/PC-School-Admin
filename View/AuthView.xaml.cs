faea7bc38af95996cdc10c1aaa841f59236b1ab1
GreshnikAlex <1995BLADE@mail.ru>
2019-02-28 19:39:16 +0200

    - Настройка страницы авторизации

---
+++ View/AuthView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PC_School_Admin.View
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthView.xaml
+    /// </summary>
+    public partial class AuthView : Page
+    {
+        /// <summary>
+        /// Конструктор страницы AuthView
+        /// </summary>
+        public AuthView() => InitializeComponent();
+    }
+}
