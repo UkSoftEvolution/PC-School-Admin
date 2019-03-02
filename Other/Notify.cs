bac0ad4dfd838e05b3f09eab5c12f31360f9deaa
GreshnikAlex <1995BLADE@mail.ru>
2019-03-02 18:55:34 +0200

    - Логика приложения для работы в трее

---
+++ Other/Notify.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Forms;
+
+namespace PC_School_Admin.Other
+{
+    /// <summary>
+    /// Класс для работы программы в трее
+    /// </summary>
+    public class Notify
+    {
+        #region Fields
+        private NotifyIcon notifyIcon; //Работа с треем
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Стандартный конструктор для инициализации данных
+        /// </summary>
+        public Notify()
+        {
+            notifyIcon = new NotifyIcon();
+            notifyIcon.Text = "PC School Admin";
+            notifyIcon.Icon = Properties.Resources.School;
+            notifyIcon.Visible = true;
+        }
+        #endregion
+    }
+}
